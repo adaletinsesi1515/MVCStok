083f86d2260ed4081a938cc9c42841e7117eb77f
Memduh Gümen <75331205+adaletinsesi1515@users.noreply.github.com>
2021-03-14 21:42:28 +0300

    GÜNCELLEME2

--- MVCStok/Controllers/UrunController.cs
+++ MVCStok/Controllers/UrunController.cs
@@ -20,12 +20,14 @@ namespace MVCStok.Controllers
         [HttpGet]
         public ActionResult Yeniurun()
         {
-            List<SelectListItem> degerler = (from i in db.TBLKATEGORILER.ToList()
-                                             select new SelectListItem
-                                             {
-                                                 Text = i.KATEGORIAD,
-                                                 Value = i.KATEGORIID.ToString()
-                                             }).ToList();
+            List<SelectListItem> degerler = (from i in db.TBLKATEGORILER.Where(i => i.DURUM == true).ToList()
+                select new SelectListItem
+                {
+
+                    Text = i.KATEGORIAD,
+                    Value = i.KATEGORIID.ToString(),
+
+                }).ToList();
             ViewBag.dgr = degerler;
             return View();
         }
@@ -37,6 +39,7 @@ namespace MVCStok.Controllers
             p1.TBLKATEGORILER = ktg;
 
             db.TBLURUNLER.Add(p1);
+            p1.DURUM = true;
             db.SaveChanges();
             return RedirectToAction("Index");
         }
@@ -45,6 +48,36 @@ namespace MVCStok.Controllers
         {
             var urun = db.TBLURUNLER.Find(id);
             urun.DURUM = false;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult UrunGetir(int id)
+        {
+            var urun = db.TBLURUNLER.Find(id);
+            List<SelectListItem> degerler = (from i in db.TBLKATEGORILER.Where(i=>i.DURUM==true).ToList()
+                select new SelectListItem
+                {
+                    
+                    Text = i.KATEGORIAD,
+                    Value = i.KATEGORIID.ToString(),
+
+                }).ToList();
+            ViewBag.dgr = degerler;
+
+            return View("UrunGetir", urun);
+        }
+
+        public ActionResult Guncelle(TBLURUNLER p)
+        {
+            var urunler = db.TBLURUNLER.Find(p.URUNID);
+            urunler.URUNAD = p.URUNAD;
+            urunler.MARKA = p.MARKA;
+            urunler.FIYAT = p.FIYAT;
+            urunler.STOK = p.STOK;
+            urunler.DURUM = true;
+            var ktg = db.TBLKATEGORILER.Where(m => m.KATEGORIID == p.TBLKATEGORILER.KATEGORIID).FirstOrDefault();
+            urunler.URUNKATEGORI = ktg.KATEGORIID;
             db.SaveChanges();
             return RedirectToAction("Index");
         }
